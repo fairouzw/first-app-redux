4149707371e24dcb16d40bbce0e7290b087e2fd2
fairouz <fairouzkoehlerwest@gmail.com>
2020-01-23 21:23:38 +0100

    makes minor changes

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,43 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import {getWeatherData} from './weatherAction';
+import { getWeatherData } from "./weatherAction";
 
 class Weather extends Component {
   constructor() {
     super();
     this.state = {
       city: ""
-    }
+    };
   }
-  inputHandler = (e) =>{
+  inputHandler = e => {
     this.setState({
       city: e.target.value
-    })
-  }
+    });
+  };
 
   clickHandler = () => {
-    this.props.getWeatherData(this.state.city)
+    this.props.getWeatherData(this.state.city);
   };
-  
+
   // componentWillMount() {
   //   this.props.getWeatherData('berlin')
   // }
 
   render() {
-    console.log(this.props)
+    console.log(this.props);
     return (
       <div>
-        <input type="text" value={this.state.city} onChange={this.inputHandler}/>
-        <button onClick={this.clickHandler}> Click</button>
-  <h1> The weather in {this.props.name} is {this.props.mainWeather && this.props.mainWeather.temp} degrees </h1>
+        <input
+          type="text"
+          value={this.state.city}
+          onChange={this.inputHandler}
+        />
+        <button onClick={this.clickHandler}> Get weather!</button>
+        <h1>
+          {" "}
+          The weather in {this.props.name} is{" "}
+          {this.props.mainWeather && this.props.mainWeather.temp} degrees{" "}
+        </h1>
       </div>
-    )
+    );
   }
 }
 
 const MapStateToProps = state => ({
   name: state.weatherReducer.weatherData.name,
   mainWeather: state.weatherReducer.weatherData.main
-})
+});
 
-export default connect(MapStateToProps, {getWeatherData}) (Weather); 
+export default connect(MapStateToProps, { getWeatherData })(Weather);
